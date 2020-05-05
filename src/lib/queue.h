bc3b3a26e70895e66f882c840f68e9e609c5e698
Janis Peyer <jpeyer@student.ethz.ch>
2020-05-05 11:48:40 +0200

    Implemented simple queue in C

---
+++ src/lib/queue.h
@@ -0,0 +1,25 @@
+/**
+ * \file Simple C queue.
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+struct queue_node {
+    void *data;
+    struct queue_node *next;
+};
+
+struct queue {
+    struct queue_node *front, *back;
+};
+
+struct queue make_queue(void);
+
+bool queue_empty(const struct queue *q);
+
+void enqueue(struct queue *q, void *entry);
+
+void *dequeue(struct queue *q);
+
+#endif  // QUEUE_H
