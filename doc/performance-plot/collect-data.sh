316de7990b6c7b1f5c3a8366e35eb1519c40a2f0
Pascal Huber <pahuber@student.ethz.ch>
2020-05-06 10:11:34 +0200

    remove block size parameter from performance-plot script

--- doc/performance-plot/collect-data.sh
+++ doc/performance-plot/collect-data.sh
@@ -7,7 +7,7 @@ echo "$dir"
 for img in lena_64 monkey_128 lena_256 lena_512 grey-parrot_1024
 do
   echo "$img start (`date +"%Y-%m-%dT %H:%M:%S"`)"
-  ./../../build/fic -b -c -i 10 -s 8 16 \
+  ./../../build/fic -b -c -i 10 \
                     -f "${dir}/../../images/${img}.gray" \
                     -o "${dir}/data/${img}.csv"
   echo "$img end (`date +"%Y-%m-%dT %H:%M:%S"`)"
