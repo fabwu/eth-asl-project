e6aa968233a5b2aa607d13070789b7afce4e024a
Pascal Huber <pahuber@student.ethz.ch>
2020-05-09 17:45:53 +0200

    precompute indices

--- doc/performance-plot/README.md
+++ doc/performance-plot/README.md
@@ -8,3 +8,8 @@ Run `./collect-data.sh` and `Rscript performance.R` to create the plot.
 install.packages("ggplot2")
 install.packages("svglite")
 ```
+
+## Add new executable
+
+ - Add it to the outer for-loop in `collect-data.sh`
+ - Add it to the Rscript
--- doc/valgrind/grind.sh
+++ doc/valgrind/grind.sh
@@ -9,8 +9,15 @@ fic_opts="-c -d -e 100 -f images/lena_256.gray"
 # baseline
 valgrind --tool=cachegrind \
          --cachegrind-out-file="./doc/valgrind/logs/cachegrind-baseline.log" \
-         --log-file="./doc/valgrind/logs/cachegrind-baseline.log" \
          ./build/baseline $fic_opts &
 valgrind --tool=callgrind \
          --callgrind-out-file="./doc/valgrind/logs/callgrind-baseline.log" \
          ./build/baseline $fic_opts &
+
+# 01_precompute_indices
+valgrind --tool=cachegrind \
+         --cachegrind-out-file="./doc/valgrind/logs/cachegrind-01_precompute_indices.log" \
+         ./build/01_precompute_indices $fic_opts &
+valgrind --tool=callgrind \
+         --callgrind-out-file="./doc/valgrind/logs/callgrind-01_precompute_indices.log" \
+         ./build/01_precompute_indices $fic_opts &
