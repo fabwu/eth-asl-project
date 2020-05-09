a8d8d4931c8e14fe6090bc925fec8f2b75a8abfb
Fabian Wüthrich <me@fabwu.ch>
2020-05-09 15:56:01 +0200

    Use macro for flop counter

--- src/lib/performance.c
+++ src/lib/performance.c
@@ -1,17 +1,8 @@
-#include "performance.h"
-
-static long long int nbr_double_flops = 0;
-
-void __reset_flop_counter() {
-#ifdef ENABLE_PERF_COUNTER
-    nbr_double_flops = 0;
-#endif
-}
-
-void __record_double_flops(const int amount) {
-#ifdef ENABLE_PERF_COUNTER
-    nbr_double_flops += amount;
-#endif
-}
-
-long long int __nbr_double_flops(void) { return nbr_double_flops; }
+/**
+ * Allocate memory for global flops counter once
+ *
+ * Use extern keyword in header so other files can see counter
+ *
+ * see https://stackoverflow.com/a/3010658/4143128
+ */
+long long int nbr_double_flops = 0;
