88ea909657286337f596e066c5eaf3aa9af20a70
Pascal Huber <pahuber@student.ethz.ch>
2020-06-03 10:03:54 +0200

    Do some comiler flag benchmarks

---
+++ doc/compiler-flags/compiler-flags.R
@@ -0,0 +1,197 @@
+library(ggplot2)
+library(svglite)
+
+executables = c(
+    ## "25_ilp"
+    ## "35_simd"
+    ## "0_baseline",
+    ## "31_simd_precomp_rotations_no_bac_simd",
+    ## "40_ilp_norot_90_270"
+    "41_simd_norot_90_270"
+)
+
+flags = c(
+  "gcc_O3",
+  "icc_O3",
+  ## "gcc_O3_fma",
+  ## "gcc_O3_fma_unroll",
+  "gcc_Ofast_fma",
+  "icc_Ofast_fma"
+)
+
+## one
+    ## "0_baseline",
+    ## "25_ilp",
+    ## "35_simd"
+
+## two
+    ## "0_baseline",
+    ## "31_simd_precomp_rotations_no_bac_simd",
+    ## "40_ilp_norot_90_270",
+    ## "41_simd_norot_90_270"
+
+
+## "35_simd",
+## "31_simd_precomp_rotations_no_bac_simd",
+## "40_ilp_norot_90_270",
+## "41_simd_norot_90_270"
+
+## 31=better-memory-access
+## 40=ILP_without_90/270
+## 41=SIMD_without_90/270
+
+image_sizes = c(
+    64,
+    128,
+    256,
+    512,
+    1024,
+    2048,
+    4096
+)
+
+## collect data
+df = data.frame()
+for (exe in executables) {
+  for(flag in flags){
+
+    ## read files
+    files = c(
+      ## new images
+      ## paste("data/", exe, "/lion_64.csv", sep=""),
+      ## paste("data/", exe, "/lion_128.csv", sep=""),
+      ## paste("data/", exe, "/lion_256.csv", sep=""),
+      ## paste("data/", exe, "/lion_512.csv", sep=""),
+      ## paste("data/", exe, "/lion_1024.csv", sep=""),
+      ## paste("data/", exe, "/lion_2048.csv", sep=""),
+      ## paste("data/", exe, "/lion_4096.csv", sep="")
+
+      paste("data/", flag, "-", exe, "/lion_64.csv", sep=""),
+      paste("data/", flag, "-", exe, "/lion_128.csv", sep=""),
+      paste("data/", flag, "-", exe, "/lion_256.csv", sep=""),
+      paste("data/", flag, "-", exe, "/lion_512.csv", sep=""),
+      paste("data/", flag, "-", exe, "/lion_1024.csv", sep=""),
+      paste("data/", flag, "-", exe, "/lion_2048.csv", sep=""),
+      paste("data/", flag, "-", exe, "/lion_4096.csv", sep="")
+    )
+
+    for(i in 1:7){
+      if(file.exists(files[i])){
+        csv = read.csv(files[i], sep=';')
+        df_temp = data.frame(
+          paste(flag, "-", exe),
+          image_sizes[i],
+          mean(csv$flops.cycle),
+          mean(csv$cycles),
+          mean(csv$flops)
+        )
+        df = rbind(df, df_temp)
+      } else {
+        print(paste("doesn't exist:", files[i]))
+      }
+    }
+  }
+
+}
+colnames(df) = c("executable", "size", "performance", "cycles", "flops")
+
+
+# baseplot
+
+baseplot = ggplot(data=df,
+                  aes(x=size,
+                      group=executable,
+                      color=executable)) +
+    scale_x_continuous(
+        name="Image Size (n x n)",
+        expand = c(0, 20)
+    ) +
+  theme_minimal() +
+    theme(
+        axis.title.y = element_text(angle=0),
+        axis.text.x = element_text(size=24),
+        axis.text.y = element_text(size=24),
+        ## legend.position = "none",
+      legend.title = element_blank())
+
+
+## Performance plot
+performance_plot = baseplot +
+    geom_line(aes(y=performance), lwd=2) +
+    geom_point(aes(y=performance), lwd=4) +
+    ggtitle("i7-8650U @ 1.9 GHz") +
+    scale_y_continuous(name=("[flops/cycle]"), limits=c(0,4))
+
+    ## geom_label(aes(x=1800, y=2.4, label = "-O3"),
+    ##            label.size = NA,
+    ##            color = "#00B937", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=1600, y=3.2, label = "-Ofast -funsafe-math -ffast-math"),
+    ##            label.size = NA,
+    ##            color = "#609BFE", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=1500, y=0.8, label = "No flags"),
+    ##            label.size = NA,
+    ##            color = "#F7756C", hjust=-1/32, vjust=1, lwd=8)
+
+
+    ## geom_label(aes(x=1500, y=1.7, label = "Scalar"),
+    ##            label.size = NA,
+    ##            color = "#00B937", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=1500, y=2.4, label = "SIMD"),
+    ##            label.size = NA,
+    ##            color = "#609BFE", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=1500, y=0.8, label = "Baseline"),
+    ##            label.size = NA,
+    ##            color = "#F7756C", hjust=-1/32, vjust=1, lwd=8)
+
+
+
+    ## geom_label(aes(x=1300, y=1.95, label = "Copy Memory"),
+    ##            label.size = NA,
+    ##            color = "#7BAD00", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=900, y=3.7, label = "SIMD (without 90/270)"),
+    ##            label.size = NA,
+    ##            color = "#C67BFE", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=900, y=1.3, label = "Scalar (without 90/270)"),
+    ##            label.size = NA,
+    ##            color = "#00BEC3", hjust=-1/32, vjust=1, lwd=8) +
+    ## geom_label(aes(x=1000, y=0.45, label = "Baseline"),
+    ##            label.size = NA,
+    ##            color = "#F7756C", hjust=-1/32, vjust=1, lwd=8)
+## ggsave(file="performance.png",
+##        dpi = 300,
+##        plot=performance_plot,
+##        width=15,
+##        height=8)
+
+## Runtime plot
+runtime_plot = baseplot +
+    ggtitle("Runtime") +
+    geom_line(aes(y=cycles), lwd=2) +
+    geom_point(aes(y=cycles), lwd=4) +
+    scale_y_continuous(name=("[cycles]"))
+
+## Flops plot
+flops_plot = baseplot +
+    ggtitle("Flops") +
+    geom_line(aes(y=flops), lwd=2) +
+    geom_point(aes(y=flops), lwd=4) +
+    scale_y_continuous(name=("[flops]"))
+
+## Save plots
+ggsave(file="performance.pdf",
+       dpi = 300,
+       plot=performance_plot,
+       width=10,
+       height=6)
+
+ggsave(file="runtime.pdf",
+       dpi = 300,
+       plot=runtime_plot,
+       width=15,
+       height=8)
+
+ggsave(file="flops.pdf",
+       dpi = 300,
+       plot=flops_plot,
+       width=15,
+       height=8)
--- doc/compiler-flags/test-compiler-flags.sh
+++ doc/compiler-flags/test-compiler-flags.sh
@@ -1,109 +1,150 @@
 #!/bin/bash
 
+REPETITIONS=2
+images=("lion_64" "lion_128" "lion_256" "lion_512" "lion_1024" "lion_2048" "lion_4096")
+
+# test images
+# images=("lion_64" "lion_128")
+
 dir=$(dirname "$(readlink -f "$0")")
 cd "$dir/../../" || exit 1
 
+runbenchmarks40(){
+      cmake --build build --target 40_ilp_norot_90_270
+      name="$1"
+      exe="40_ilp_norot_90_270"
+      for img in ${images[@]}
+      do
+            echo "--------------------------------"
+            echo "[$img - $exe] start (`date +"%Y-%m-%dT %H:%M:%S"`)"
+            mkdir -p "${dir}/data/${name}-${exe}"
+            ${dir}/../../build/${exe} \
+                  -b -c -i 10 \
+                  -r "$REPETITIONS" \
+                  -f "${dir}/../../images/${img}.gray" \
+                  -o "${dir}/data/${name}-${exe}/${img}.csv"
+      done
+}
+
+
+runbenchmarks41(){
+      cmake --build build --target 41_simd_norot_90_270
+      name="$1"
+      exe="41_simd_norot_90_270"
+      for img in ${images[@]}
+      do
+            echo "--------------------------------"
+            echo "[$img - $exe] start (`date +"%Y-%m-%dT %H:%M:%S"`)"
+            mkdir -p "${dir}/data/${name}-${exe}"
+            ${dir}/../../build/${exe} \
+                  -b -c -i 10 \
+                  -r "$REPETITIONS" \
+                  -f "${dir}/../../images/${img}.gray" \
+                  -o "${dir}/data/${name}-${exe}/${img}.csv"
+      done
+}
+
 echo "----------------------------"
-echo "  set no flags"
+echo "  gcc_Ofast_fma"
 echo "----------------------------"
 cmake -DCMAKE_BUILD_TYPE=Release \
-      -DOPT_ARCH=NO \
+      -DICC=NO \
+      -DOPT_ARCH=YES \
       -DOPT_O1=NO \
       -DOPT_O2=NO \
       -DOPT_O3=NO \
-      -DOPT_OFAST=NO \
-      -DOPT_VEC=NO \
-      -DOPT_UNSAFE_MATH=NO \
-      -DOPT_UNROLL_LOOPS=NO \
-      -DOPT_FAST_MATH=NO \
-      -B build/
-cmake --build build --target 37_full_simd
-./build/37_full_simd -b -c -r 1 -f images/lena_512.gray
-
-
-echo "----------------------------"
-echo "  add O3"
-echo "----------------------------"
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DOPT_ARCH=NO \
-      -DOPT_O1=NO \
-      -DOPT_O2=NO \
-      -DOPT_O3=YES \
-      -DOPT_OFAST=NO \
-      -DOPT_VEC=NO \
-      -DOPT_UNSAFE_MATH=NO \
-      -DOPT_UNROLL_LOOPS=NO \
-      -DOPT_FAST_MATH=NO \
-      -B build/
-cmake --build build --target 37_full_simd
-./build/37_full_simd -b -c -r 1 -f images/lena_512.gray
-
-# fast but  still stable
-echo "----------------------------"
-echo "  now also vectorize"
-echo "----------------------------"
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DOPT_ARCH=YES \
-      -DOPT_O1=NO \
-      -DOPT_O2=NO \
-      -DOPT_O3=YES \
-      -DOPT_OFAST=NO \
-      -DOPT_VEC=YES \
-      -DOPT_UNSAFE_MATH=NO \
-      -DOPT_UNROLL_LOOPS=NO \
-      -DOPT_FAST_MATH=NO \
-      -B build/
-cmake --build build --target 37_full_simd
-./build/37_full_simd -b -c -r 1 -f images/lena_512.gray
-
-echo "----------------------------"
-echo "  now also unroll loop"
-echo "----------------------------"
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DOPT_ARCH=YES \
-      -DOPT_O1=NO \
-      -DOPT_O2=NO \
-      -DOPT_O3=YES \
-      -DOPT_OFAST=NO \
-      -DOPT_VEC=YES \
-      -DOPT_UNSAFE_MATH=NO \
-      -DOPT_UNROLL_LOOPS=YES \
-      -DOPT_FAST_MATH=YES \
-      -B build/
-
-echo "----------------------------"
-echo "  set fast flags"
-echo "----------------------------"
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DOPT_ARCH=YES \
-      -DOPT_O1=NO \
-      -DOPT_O2=NO \
-      -DOPT_O3=YES \
-      -DOPT_OFAST=NO \
-      -DOPT_VEC=YES \
-      -DOPT_UNSAFE_MATH=YES \
-      -DOPT_UNROLL_LOOPS=YES \
-      -DOPT_FAST_MATH=YES \
-      -B build/
-
-cmake --build build --target 37_full_simd
-./build/37_full_simd -b -c -r 1 -f images/lena_512.gray
-
-
-echo "----------------------------"
-echo "  set fast flags"
-echo "----------------------------"
-cmake -DCMAKE_BUILD_TYPE=Release \
-      -DOPT_ARCH=YES \
-      -DOPT_O1=NO \
-      -DOPT_O2=NO \
-      -DOPT_O3=YES \
       -DOPT_OFAST=YES \
-      -DOPT_VEC=YES \
-      -DOPT_UNSAFE_MATH=YES \
-      -DOPT_UNROLL_LOOPS=YES \
-      -DOPT_FAST_MATH=YES \
+      -DOPT_FMA=YES \
+      -DOPT_UNROLL_LOOPS=NO \
       -B build/
+runbenchmarks40 "gcc_Ofast_fma"
+runbenchmarks41 "gcc_Ofast_fma"
 
-cmake --build build --target 37_full_simd
-./build/37_full_simd -b -c -r 1 -f images/lena_512.gray
+
+echo "----------------------------"
+echo "  gcc_03_fma_unroll "
+echo "----------------------------"
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DICC=NO \
+      -DOPT_O1=NO \
+      -DOPT_O2=NO \
+      -DOPT_O3=YES \
+      -DOPT_OFAST=NO \
+      -DOPT_FMA=YES \
+      -DOPT_UNROLL_LOOPS=YES \
+      -B build/
+runbenchmarks40 "gcc_O3_fma_unroll"
+runbenchmarks41 "gcc_O3_fma_unroll"
+
+
+echo "----------------------------"
+echo "  gcc_O3_fma"
+echo "----------------------------"
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DICC=NO \
+      -DOPT_ARCH=YES \
+      -DOPT_O1=NO \
+      -DOPT_O2=NO \
+      -DOPT_O3=YES \
+      -DOPT_OFAST=NO \
+      -DOPT_FMA=YES \
+      -DOPT_UNROLL_LOOPS=NO \
+      -B build/
+runbenchmarks40 "gcc_O3_fma"
+runbenchmarks41 "gcc_O3_fma"
+
+
+echo "----------------------------"
+echo "  gcc_03"
+echo "----------------------------"
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DICC=NO \
+      -DOPT_O1=NO \
+      -DOPT_O2=NO \
+      -DOPT_O3=YES \
+      -DOPT_OFAST=NO \
+      -DOPT_FMA=NO \
+      -DOPT_UNROLL_LOOPS=NO \
+      -B build/
+runbenchmarks40 "gcc_O3"
+# no fma runbenchmarks41 "gcc_O3"
+
+
+echo "############################"
+echo "############################"
+echo "         ICC"
+echo "############################"
+echo "############################"
+
+
+echo "----------------------------"
+echo "  icc_Ofast_fma"
+echo "----------------------------"
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DICC=YES \
+      -DOPT_ARCH=YES \
+      -DOPT_O1=NO \
+      -DOPT_O2=NO \
+      -DOPT_O3=NO \
+      -DOPT_OFAST=YES \
+      -DOPT_FMA=YES \
+      -DOPT_UNROLL_LOOPS=NO \
+      -B build/
+runbenchmarks41 "icc_Ofast_fma"
+runbenchmarks40 "icc_Ofast_fma"
+
+
+echo "----------------------------"
+echo "  icc_03"
+echo "----------------------------"
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DICC=NO \
+      -DOPT_O1=NO \
+      -DOPT_O2=NO \
+      -DOPT_O3=YES \
+      -DOPT_OFAST=NO \
+      -DOPT_FMA=NO \
+      -DOPT_UNROLL_LOOPS=NO \
+      -B build/
+runbenchmarks40 "icc_O3"
+# no fma runbenchmarks41 "icc_O3"
